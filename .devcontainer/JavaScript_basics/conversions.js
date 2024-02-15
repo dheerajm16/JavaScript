ed6d2e461a74e88e1e160ebf8801b1e11db8caaa
dheerajm16 <78255931+dheerajm16@users.noreply.github.com>
2024-02-15 23:15:06 +0000

    concat

--- .devcontainer/JavaScript_basics/conversions.js
+++ .devcontainer/JavaScript_basics/conversions.js
@@ -1,17 +1,23 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof (score))
+// console.log(typeof (score))
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-let loggedIn = 1;
+// let loggedIn = 1;
 
-let boolLoogedIn = Boolean(loggedIn)
-console.log(boolLoogedIn)
+// let boolLoogedIn = Boolean(loggedIn)
+// console.log(boolLoogedIn)
 
-let number = 33
-let stringNumber = String(number)
-console.log(stringNumber)
+// let number = 33
+// let stringNumber = String(number)
+// console.log(stringNumber)
+
+let str1 = "Hello "
+let str2 = "World"
+let str3 = str1 + str2
+
+console.log(str3)
